0a2621adc710d8727df0a07d68fca9721ce44e6d
abu <developerabu@gmail.com>
2021-03-27 03:32:12 +0000

    add pagination

--- signals_app/models.py
+++ signals_app/models.py
@@ -10,5 +10,8 @@ class Signals(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.crypto_symbol
+
     class Meta:
         verbose_name_plural = "signals"
